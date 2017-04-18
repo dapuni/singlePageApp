a21cadb26cb7e66511fd09c63dda80b052953fec
Irvani Altha <irvani.altha@elnusa.co.id>
2017-04-18 14:42:44 +0700

    update Route

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use App\roles;
 
 // Authentication Routes...
 Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('/login', 'Auth\LoginController@login');
+//Route::post('/login', 'Auth\LoginController@login');
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/', function () {
